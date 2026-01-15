7067709a39dcb2389d7d1e859ed7e56049eb8745
admin <daniel_brito@epam.com>
2026-01-15 00:30:06 -0300

    Move aws_iam_policy from tf_code_1 to tf_code_2 using terraform state mv

--- tf_code_2/main.tf
+++ tf_code_2/main.tf
@@ -1,12 +1,3 @@
-resource "aws_iam_policy" "this" {
-  name = var.policy_name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["s3:ListAllMyBuckets"]
-      Resource = "*"
-    }]
-  })
+provider "aws" {
+  region = var.region
 }
---
+++ tf_code_2/resources.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_policy" "this" {
+  name = var.policy_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:ListAllMyBuckets"]
+      Resource = "*"
+    }]
+  })
+}
