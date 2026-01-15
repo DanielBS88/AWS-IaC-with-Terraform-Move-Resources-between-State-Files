aed14f2584a42cfd70f945ca71718ddbc2b87249
admin <daniel_brito@epam.com>
2026-01-15 00:09:21 -0300

    Initial Terraform setup and state migration (aws_iam_policy)

---
+++ tf_code_1/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "policy_name" {
+  description = "Name of the IAM policy"
+  type        = string
+}
---
+++ tf_code_2/terraform.tfvars
@@ -0,0 +1,2 @@
+region      = "us-east-1"
+policy_name = "custom_policy"
